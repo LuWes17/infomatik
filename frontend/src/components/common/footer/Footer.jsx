a52928114806a7555d85f2eb2bcfa0d0f6a72762
gheelopez <ghee_lopez@dlsu.edu.ph>
2025-08-24 18:33:20 +0800

    feat: completed footer details

--- frontend/src/components/common/footer/Footer.jsx
+++ frontend/src/components/common/footer/Footer.jsx
@@ -5,40 +5,111 @@ const Footer = () => {
     return(
         <footer className={styles.footer}>
             <div className="container">
+            {/* Contact Us Section */}
+        <div className={styles.contactSection}>
+          <div className={styles.contactHeader}>
+            <div className={styles.contactusContainer}>
+              <img 
+                src={contactus} 
+                alt="contact-us" 
+                className={styles.contactIcon}
+              />
+            </div>
+            Contact Us:
+          </div>
+        
+          <div className={styles.hotlinesGrid}>
+            {/* Office */}
+            <div className={styles.contactItem}>
+                <div className={styles.contactItem}>
+                  <span className={styles.hotlineLabel}>Office of the Councilor:</span>
+                  <span className={styles.hotlineDetail}>&nbsp; 0920 211 7274</span>
+                </div>
+            </div>
+        
+            {/* Facebook */}
+            <div className={styles.contactItem}>
+              <div className={styles.hotlineLabel}>Facebook:</div>
+              <span className={styles.hotlineDetail}>&nbsp; fb.com/councilorbon</span>
+            </div>
+        
+            {/* Gmail */}
+            <div className={styles.contactItem}>
+              <div className={styles.hotlineLabel}>Gmail:</div>
+              <span className={styles.hotlineDetail}>&nbsp; gmail@gmail.com</span>
+            </div>
+          </div>
+        </div>
+            
+            
                 <div className={styles.emergencySection}>
                     <div className={styles.emergencyHeader}>
                         <div className={styles.contactusContainer}>
-                                    <Link to="/">
-                                      <img 
-                                        src={contactus} 
-                                        alt="contact-us" 
-                                        className={styles.contactIcon}
-                                      />
-                                    </Link>
-                                  </div>
+                            <img 
+                                src={contactus} 
+                                alt="contact-us" 
+                                className={styles.contactIcon}
+                            />
+                        </div>
                         Emergency Hotlines:
                     </div>
 
-                    <div className={styles.hotlinesGrid}>
-                        <div className={styles.hotlineItem}>
-                            <div className={styles.hotlineContent}>CDRMMO Operations Center and Ambulance Services: 0995 290 5288   0939 782 9833  0909 224 5858  0999 475 8582</div>
+                <div className={styles.hotlinesGrid}>
+                {/* CDRMMO */}
+                <div className={styles.hotlineItem}>
+                    <div className={styles.hotlineLabel}>
+                          CDRMMO Operations Center and Ambulance Services
+                    </div>
+                    <div className={styles.hotlineNumbers}>
+                        <span>• 0995 290 5288</span>
+                        <span>• 0939 782 9833</span>
+                        <span>• 0909 224 5858</span>
+                        <span>• 0999 475 8582</span>
+                    </div>
+                </div>
+                
+                {/* BFP */}
+                <div className={styles.hotlineItem}>
+                    <div className={styles.hotlineLabel}>Bureau of Fire Protection Tabaco City</div>
+                        <div className={styles.hotlineNumbers}>
+                            <span>• 0915 837 5720</span>
+                            <span>• 0939 915 1789</span>
                         </div>
-                        <div className={styles.hotlineItem}>
-                            <div className={styles.hotlineLabel}>Office of the Councilor: 09xx xxx xxxx</div>
+                </div>
+                
+                {/* PNP Tabaco */}
+                <div className={styles.hotlineItem}>
+                    <div className={styles.hotlineLabel}>Philippine National Police Tabaco City</div>
+                    <div className={styles.hotlineNumbers}>
+                        <span>• 0998 598 5930</span>
+                        <span>• 0917 658 2462</span>
+                    </div>
+                </div>
+                
+                {/* Public City Office */}
+                <div className={styles.hotlineItem}>
+                    <div className={styles.hotlineLabel}>Public City Office</div>
+                    <div className={styles.hotlineNumbers}>
+                        <span>• 0995 530 5726</span>
+                    </div>
+                </div>
+                
+                {/* Coastguard */}
+                <div className={styles.hotlineItem}>
+                    <div className={styles.hotlineLabel}>Coastguard Tabaco City</div>
+                        <div className={styles.hotlineNumbers}>
+                            <span>• 0963 375 3396</span>
                         </div>
-                        <div className={styles.hotlineItem}>
-                            <div className={styles.hotlineLabel}>Office of the Councilor: 09xx xxx xxxx</div>
-                        </div>
-                        <div className={styles.hotlineItem}>
-                            <div className={styles.hotlineLabel}>Office of the Councilor: 09xx xxx xxxx</div>
-                        </div>
-                        <div className={styles.hotlineItem}>
-                            <div className={styles.hotlineLabel}>Office of the Councilor: 09xx xxx xxxx</div>
-                        </div>
-                        <div className={styles.hotlineItem}>
-                            <div className={styles.hotlineLabel}>Office of the Councilor: 09xx xxx xxxx</div>
+                </div>
+                
+                {/* PNP Maritime */}
+                <div className={styles.hotlineItem}>
+                    <div className={styles.hotlineLabel}>Philippine National Police Maritime Tabaco City</div>
+                        <div className={styles.hotlineNumbers}>
+                            <span>• 0906 041 5925</span>
                         </div>
                     </div>
+                </div>
                 </div>
 
                 <div className={styles.divider}></div>
